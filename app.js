f5c43a64227d9cf7012663bec254f15443f5744d
djrenot <kiritori.ch@gmail.com>
2021-06-05 23:52:08 +0900

    added fetch but not working :(

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const request = axios.create({
-  baseURL: 'https://api.github.com'
-})
+// const request = axios.create({
+//   baseURL: 'https://api.github.com'
+// })
+
+
 
 // get user data
 // request.get('/users/djrenot')
@@ -8,19 +10,30 @@ const request = axios.create({
 //   .then(console.log)
 
 // get user repos
-request.get('/users/djrenot/repos')
-  .then(res => res.data)
-  .then(repos => {
-    let list = document.getElementById('list-repos')
-    // console.log(repos);
-    console.log(list);
-    for(let i=0;i<repos.length;i++){
-      // console.log(repos[i].name)
-      let list_item = document.createElement("LI");
-      list_item.innerHTML = repos[i].name;
-      list.appendChild(list_item);
-    }
-  })
+// request.get('/users/djrenot/repos')
+//   .then(res => res.data)
+//   .then(repos => {
+//     let list = document.getElementById('list-repos')
+//     // console.log(repos);
+//     console.log(list);
+//     for(let i=0;i<repos.length;i++){
+//       // console.log(repos[i].name)
+//       let list_item = document.createElement("LI");
+//       list_item.innerHTML = repos[i].name;
+//       list.appendChild(list_item);
+//     }
+//   })
+
+// just use fetch
+let fetch_repo = await fetch('https://api.github.com/users/djrenot/repos')
+let repo_list = await fetch_repo.json();
+// for(let i=0;i<repo_list.length;i++){
+//   // console.log(repo_list[i].name)
+//   let list_item = document.createElement("LI");
+//   list_item.innerHTML = repo_list[i].name;
+//   list.appendChild(list_item);
+// }
+
 
   // scroll to top of the page
 function scroll_top() {
