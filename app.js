b4e6c153a81db3522effe3e466860a5080f7e07e
djrenot <kiritori.ch@gmail.com>
2021-06-08 22:53:14 +0900

    removed unused scripts

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ const request = axios.create({
   baseURL: 'https://api.github.com'
 })
 
-// get user data
-// request.get('/users/djrenot')
-//   .then(res => res.data)
-//   .then(console.log)
-
 // get user repos
 request.get('/users/djrenot/repos')
   .then(res => res.data)
