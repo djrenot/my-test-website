811f8dc2b6519c3c2bdf8595a0fda12d366ec4df
djrenot <kiritori.ch@gmail.com>
2021-06-09 23:37:37 +0900

    updated each item has own link to the repo + show numbers of repos

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 async function github_repo(){
   let fetch_repo = await fetch('https://api.github.com/users/djrenot/repos')
   let repo_list = await fetch_repo.json();
-  let list = document.getElementById('list-repos')
+  let list = document.getElementById('list-repos');
+
+  let repo_num = document.getElementById('repo-num');
+  repo_num.innerHTML = repo_list.length;
 
   for(let i=0;i<repo_list.length;i++){
     // console.log(repo_list[i].name)
     let list_item = document.createElement("LI");
-    list_item.innerHTML = repo_list[i].name;
+    list_item.innerHTML = '<a href="' + repo_list[i].html_url + '" target=_blank>' + repo_list[i].name + '</a>' ;
     list.appendChild(list_item);
   }
 }
